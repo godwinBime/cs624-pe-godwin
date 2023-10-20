377e20aaec6d9c6e6dcb82c4e29fe43e62d24a58
godwinBime <74328112+godwinBime@users.noreply.github.com>
2023-10-20 13:05:42 +0000

    Submission PE03 - Godwin Bime

--- PE03-todos /todos/App.js
+++ PE03-todos /todos/App.js
@@ -2,6 +2,9 @@ import React, {Component} from "react";
 import {View, ScrollView, StyleSheet} from 'react-native';
 import Heading from "./Heading";
 import Input from "./Input";
+import Button from "./Button";
+
+let todoIndex = 0;
 
 class App extends Component{
   constructor(){
@@ -11,7 +14,26 @@ class App extends Component{
       todos: [],
       type: 'All'
     };
+    this.submitTodo = this.submitTodo.bind(this);
   }
+
+  submitTodo(){
+    if (this.state.inputValue.match(/^\s*$/)){
+      return "";
+    }
+    const todo = {
+      title: this.state.inputValue,
+      todoIndex,
+      compplete: false
+    }
+    todoIndex++;
+    const todos = [...this.state.todos, todo];
+    this.setState({todos, inputValue: ''}, () => {
+      console.log('State: ', this.state);
+    });
+  }
+
+
   inputChange(inputValue){
     console.log('Input Value: ', inputValue);
     this.setState({inputValue});
@@ -25,6 +47,7 @@ class App extends Component{
           <Input
           inputValue={this.state.inputValue}
           inputChange={text => this.inputChange(text)}/>
+          <Button submitTodo={this.submitTodo}/>
         </ScrollView>
       </View>
     );
